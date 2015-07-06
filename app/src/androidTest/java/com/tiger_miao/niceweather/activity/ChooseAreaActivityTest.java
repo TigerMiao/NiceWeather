c6f55164ea04518ba31ab51d6a47b67622c23a53
Tiger.miao <tiger.miao.ql@gmail.com>
2015-07-06 16:00:24 +0800

    fixed untracked filles

--- app/src/androidTest/java/com/tiger_miao/niceweather/activity/ChooseAreaActivityTest.java
+++ app/src/androidTest/java/com/tiger_miao/niceweather/activity/ChooseAreaActivityTest.java
@@ -1,7 +1,20 @@
 package com.tiger_miao.niceweather.activity;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
 /**
  * Created by tiger_miao on 15-7-4.
  */
+@RunWith(AndroidJUnit4.class)
 public class ChooseAreaActivityTest {
+
+    @Rule
+    public ActivityTestRule<ChooseAreaActivity> mActivityRule =
+            new ActivityTestRule<>(ChooseAreaActivity.class);
+
+    
 }
