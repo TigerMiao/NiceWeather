26435f516e2ca75a18a7c490e46754c683feff1e
tiger miao <tiger.miao.ql@gmail.com>
2015-07-18 11:33:05 +0800

    使用AndroidAnnotations重构了代码

--- app/src/main/java/com/tiger_miao/niceweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/tiger_miao/niceweather/activity/ChooseAreaActivity.java
@@ -1,5 +1,6 @@
 package com.tiger_miao.niceweather.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -12,15 +13,23 @@ import com.google.inject.Inject;
 import com.tiger_miao.niceweather.R;
 import com.tiger_miao.niceweather.model.IChooseAreaActivityViewModel;
 
-import roboguice.activity.RoboActivity;
-import roboguice.inject.InjectView;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.RoboGuice;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.WindowFeature;
+
 
 /**
  * Created by tiger_miao on 15-7-1.
  */
-public class ChooseAreaActivity extends RoboActivity{
-    @InjectView(R.id.title_text) private TextView titleText;
-    @InjectView(R.id.list_view) private ListView listView;
+@WindowFeature(Window.FEATURE_NO_TITLE)
+@EActivity(R.layout.choose_area)
+@RoboGuice
+public class ChooseAreaActivity extends Activity{
+    @ViewById(R.id.title_text)
+    TextView titleText;
+    @ViewById(R.id.list_view)
+    ListView listView;
 
     @Inject
     public IChooseAreaActivityViewModel viewModel;
@@ -30,12 +39,6 @@ public class ChooseAreaActivity extends RoboActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        setContentView(R.layout.choose_area);
-        titleText = (TextView) findViewById(R.id.title_text);
-        listView = (ListView) findViewById(R.id.list_view);
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, viewModel.getDataList());
         listView.setAdapter(adapter);
